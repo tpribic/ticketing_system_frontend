60d83d24b2fb635041d1db9ebf10ef86cf692efe
tpribic <tomi.pribic@gmail.com>
2020-09-30 15:58:07 +0200

    Add update issue

--- src/Components/Issues/IssueDetails/index.jsx
+++ src/Components/Issues/IssueDetails/index.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useContext, Children } from 'react'
 import { useHistory } from 'react-router-dom'
 import Axios from 'axios';
 import authHeader from '../../../Services/AuthHeader'
-import { Button, Divider, ButtonToolbar, List, Panel, PanelGroup, Content, Icon, FlexboxGrid, Loader, Form, Schema, FormGroup, ControlLabel, FormControl } from 'rsuite';
+import { Button, Divider, ButtonToolbar, List, Panel, PanelGroup, Content, Icon, FlexboxGrid, Loader, Form, Schema, FormGroup, FormControl, InputPicker } from 'rsuite';
 import priorityEnum from '../../Product/ProductIssues/priorityEnum';
+import { RoleContext } from '../../../Context/UserRoleContext';
+import { priority, isSolved } from './rsuiteConstants'
 
 const styleCenter = {
     display: 'flex',
@@ -29,19 +31,47 @@ const dataStyle = {
     fontWeight: 500
 };
 
+const BoldText = (props) => {
+    return (
+        <p style={{ fontWeight: 'bold' }}>{props.children}</p>
+    )
+}
+
 export default function IssueDetails(props) {
 
     const history = useHistory();
+    const [message, setMessage] = useState(null);
     const [issue, setIssue] = useState(undefined);
     const [comments, setComments] = useState(undefined);
     const [comment, setComment] = useState("");
     const [formError, setFormError] = useState({});
+    const [employees, setEmployees] = useState(null);
+    const [formData, setFormData] = useState({
+        issueId: parseInt(props.match.params.slug),
+        employeeId: null,
+        priority: 1,
+        isSolved: false
+    });
     const form = useRef(null);
+    const role = useContext(RoleContext);
 
     const { StringType } = Schema.Types;
     const commentModel = Schema.Model({
         comment: StringType().minLength(2, 'Comment too short.').maxLength(255, 'Too many characters.'),
     });
+
+    useEffect(() => {
+        getIssueData();
+
+    }, [])
+
+    useEffect(() => {
+        if (role.roles !== null) {
+            if (role.roles.includes("ROLE_EMPLOYEE") || role.roles.includes("ROLE_ADMIN")) {
+                getEmployees();
+            }
+        }
+    }, [role.roles])
 
 
     const getIssueData = async () => {
@@ -49,9 +79,37 @@ export default function IssueDetails(props) {
         const commentResponse = await Axios.get(process.env.REACT_APP_API_URL + `api/issue/${props.match.params.slug}/comments`, { headers: authHeader() }); setComments(commentResponse.data);
     }
 
-    useEffect(() => {
-        getIssueData();
-    }, [])
+    const updateIssueData = async () => {
+        const response = await Axios.patch(process.env.REACT_APP_API_URL + `api/employee/issue/update`, formData, { headers: authHeader() })
+            .then((response) => {
+                setIssue(response.data)
+                setMessage("Issue updated");
+            })
+            .catch((error) => {
+                setMessage(error.message)
+            });
+
+    }
+
+    const getEmployees = async () => {
+        const response = await Axios.get(process.env.REACT_APP_API_URL + `api/employee/all`, { headers: authHeader() });
+
+        let formatedData = [{
+            label: "Unasigned",
+            value: null,
+            role: "Unasigned",
+        }];
+
+        response.data.forEach(employee => {
+            formatedData.push({
+                label: employee.username,
+                value: employee.id,
+                role: "Employee",
+            })
+        });
+
+        setEmployees(formatedData);
+    }
 
     const addComment = async (event) => {
         event.preventDefault();
@@ -62,26 +120,94 @@ export default function IssueDetails(props) {
         setComment("");
         getIssueData();
     }
-
     return (
-        <Content style={{ padding: '20px' }}>
+        <Content style={{ padding: '20px', maxHeight: '92vh', overflowX: 'auto' }} >
             <Button appearance='ghost' style={{ marginBottom: 10 }} onClick={() => history.push(`/dashboard/issues`)}><Icon icon='back-arrow' /> Go to issues</Button>
             <PanelGroup accordion bordered>
                 <Panel header={issue ? `Issue name: ${issue.name}` : 'Loading Issue'} defaultExpanded collapsible={false}>
                     {issue ?
-                        <>
-                            <p><b>Issue description:</b> {issue.description}</p>
-                            <p><b>Opened by:</b> {issue.user.username}</p>
-                            <p><b>Assigned to:</b> {issue.employee ? issue.employee.username : 'Unasigned'}</p>
-                            <p><b>Priority:</b> {priorityEnum[issue.priority]}</p>
-                            <p><b>Current status:</b> {issue.solved ? 'Solved' : 'Not Solved'}</p>
-                        </>
+                        role.roles.includes("ROLE_EMPLOYEE") || role.roles.includes("ROLE_ADMIN") ?
+                            <>
+                                <FlexboxGrid style={{ justifyContent: 'flex-start' }}>
+                                    <FlexboxGrid.Item style={{ margin: '5px 10px', minWidth: 200 }}>
+                                        <BoldText>Issue description:</BoldText>
+                                        <p style={{ maxWidth: 200 }}>{issue.description}</p>
+                                    </FlexboxGrid.Item>
+                                    <FlexboxGrid.Item style={{ margin: '5px 10px', minWidth: 200 }}>
+                                        <BoldText>Opened by:</BoldText>
+                                        <p>{issue.user.username}</p>
+                                    </FlexboxGrid.Item>
+                                    <FlexboxGrid.Item style={{ margin: '5px 10px', minWidth: 200 }}>
+                                        <BoldText>Assigned to:</BoldText>
+                                        {employees ?
+                                            <InputPicker
+                                                style={{ margin: '5px 0' }}
+                                                defaultValue={issue.employee ? issue.employee.id : null}
+                                                data={employees}
+                                                block
+                                                cleanable={false}
+                                                onChange={(value) => setFormData({ ...formData, employeeId: value })}
+                                            />
+                                            :
+                                            null
+                                        }
+                                        <BoldText>Priority:</BoldText>
+                                        <InputPicker
+                                            style={{ margin: '5px 0' }}
+                                            defaultValue={issue.priority}
+                                            data={priority}
+                                            block
+                                            cleanable={false}
+                                            onChange={(value) => setFormData({ ...formData, priority: value })}
+                                        />
+                                    </FlexboxGrid.Item>
+                                    <FlexboxGrid.Item style={{ margin: '5px 10px', minWidth: 200 }}>
+                                        <BoldText>Current status:</BoldText>
+                                        <InputPicker
+                                            style={{ margin: '5px 0' }}
+                                            defaultValue={issue.solved}
+                                            data={isSolved}
+                                            block
+                                            cleanable={false}
+                                            onChange={(value) => setFormData({ ...formData, isSolved: value })}
+                                        />
+                                    </FlexboxGrid.Item>
+                                </FlexboxGrid>
+                                <div style={{ width: '100%', margin: '10px 0 5px 0' }}>
+                                    <Button
+                                        appearance={"primary"}
+                                        onClick={() => updateIssueData()}
+                                    >
+                                        Apply changes
+                                    </Button>
+                                </div>
+                                {message}
+                            </>
+                            :
+                            <FlexboxGrid style={{ justifyContent: 'space-around' }}>
+                                <FlexboxGrid.Item style={{ margin: '5px 10px', minWidth: 200 }}>
+                                    <BoldText>Issue description:</BoldText>
+                                    <p>{issue.description}</p>
+                                    <BoldText>Opened by:</BoldText>
+                                    <p>{issue.user.username}</p>
+                                </FlexboxGrid.Item>
+                                <FlexboxGrid.Item style={{ margin: '5px 10px', minWidth: 200 }}>
+                                    <BoldText>Assigned to:</BoldText>
+                                    <p>{issue.employee ? issue.employee.username : 'Unasigned'}</p>
+                                    <BoldText>Priority:</BoldText>
+                                    <p>{priorityEnum[issue.priority]}</p>
+                                </FlexboxGrid.Item>
+                                <FlexboxGrid.Item style={{ margin: '5px 10px', minWidth: 200 }}>
+                                    <BoldText>Current status:</BoldText>
+                                    <p>{issue.solved ? 'Solved' : 'Not Solved'}</p>
+                                </FlexboxGrid.Item>
+                            </FlexboxGrid>
                         :
                         <Loader />
                     }
                 </Panel>
 
-                <Panel header='Comments'>
+                <Panel header='Comments' >
                     {comments ?
                         <List style={{ maxHeight: 300, width: 'inherit' }} hover autoScroll>
                             {comments.map((item, index) => (
@@ -97,7 +223,7 @@ export default function IssueDetails(props) {
                                             />
                                         </FlexboxGrid.Item>
                                         <FlexboxGrid.Item
-                                            colspan={6}
+                                            // colspan={6}
                                             style={{
                                                 ...styleCenter,
                                                 flexDirection: 'column',
@@ -113,7 +239,8 @@ export default function IssueDetails(props) {
                                                 <div>{item['date']}</div>
                                             </div>
                                         </FlexboxGrid.Item>
-                                        <FlexboxGrid.Item colspan={1}
+                                        <FlexboxGrid.Item
+                                            // colspan={1}
                                             style={{
                                                 ...styleCenter,
                                                 flexDirection: 'column',
@@ -142,8 +269,7 @@ export default function IssueDetails(props) {
                         ref={form}
                     >
                         <FormGroup>
-                            {/* <ControlLabel>Comment</ControlLabel> */}
-                            <FormControl placeholder='Write a comment...' rows={3} name="comment" componentClass="textarea" onChange={(value) => setComment(value)} value={comment} />
+                            <FormControl style={{ minWidth: 250 }} placeholder='Write a comment...' rows={3} name="comment" componentClass="textarea" onChange={(value) => setComment(value)} value={comment} />
                         </FormGroup>
                         <ButtonToolbar>
                             <Button disabled={formError.comment !== undefined} appearance="primary" onClick={(event) => addComment(event)}>Submit</Button>
@@ -151,6 +277,7 @@ export default function IssueDetails(props) {
                     </Form>
                 </Panel>
             </PanelGroup>
-        </Content>
+            {console.log(formData)}
+        </Content >
     )
 }
---
+++ src/Components/Issues/IssueDetails/rsuiteConstants.js
@@ -0,0 +1,32 @@
+const priority = [
+  {
+    label: "High",
+    value: 1,
+    role: "High",
+  },
+  {
+    label: "Medium",
+    value: 2,
+    role: "Medium",
+  },
+  {
+    label: "Low",
+    value: 3,
+    role: "Low",
+  }
+];
+
+const isSolved = [
+  {
+    label: "Not solved",
+    value: false,
+    role: "false",
+  },
+  {
+    label: "Solved",
+    value: true,
+    role: "true",
+  }
+];
+
+export { priority, isSolved };
